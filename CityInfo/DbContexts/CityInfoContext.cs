4eb452a8b62ae5792f0edb9d66abe89f4ff10c92
Austin Jones <austin.jones@uster.com>
2024-04-16 16:28:46 -0400

    add context

--- CityInfo/DbContexts/CityInfoContext.cs
+++ CityInfo/DbContexts/CityInfoContext.cs
@@ -1,5 +1,16 @@
+using CityInfo.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace CityInfo.DbContexts;
 
 public class CityInfoContext : DbContext
 {
+    public DbSet<City> Cities { get; set; }
+    public DbSet<PointOfInterest> PointsOfInterests { get; set; }
+
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    // {
+    //     optionsBuilder.UseSqlite("connectionString");
+    //     base.OnConfiguring(optionsBuilder);
+    // }
 }
